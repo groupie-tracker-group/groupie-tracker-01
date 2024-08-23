c43994951b5f5645bac78633e9a5914f625af1c7
Oussamabenali4 <oussama.benali@ump.ac.ma>
2024-08-23 14:39:42 +0100

    adding more function to search file

--- internal/search.go
+++ internal/search.go
@@ -3,19 +3,27 @@ package data
 import "fmt"
 import "net/http"
 import "encoding/json"
-
+import "strconv"
+// run this function befor lunching the server
 func init(){
 	Decoder()
+	FillOutDataForms()
+	FillOutSearchKeys()
 }
 
-type DataBase map[string][]string
+type DataBase map[string][]int
 // define a new data base 
 func NewDataBase()*DataBase{
 	var New DataBase
 	return &New
 }
+// add element to the data
+// if the id exist scip
+func (d *DataBase)Add(key string,value int){
+	(*d)[key] = append((*d)[key], value)
+}
 // fetch an element
-func (d *DataBase)Fetch(key string)[]string{
+func (d *DataBase)Fetch(key string)[]int{
 	return (*d)[key]
 }
 
@@ -28,7 +36,7 @@ func (d *DataBase)Retrieve()[]string{
 	return sl
 }
 
-// get the data
+// get the data fromt the aip
 type TemplateData struct{
 	Artists []ArtistData
 }
@@ -52,4 +60,34 @@ func Decoder(){
 		fmt.Println("err decoding JSON:" , err)
 		return
 	}
+}
+/////////////////////////////////////////////////////
+// put the data into separate data bases
+var By_Name DataBase
+var By_Member DataBase
+var By_First_Album DataBase
+var By_Creation_Date DataBase
+
+func FillOutDataForms(){
+	for  _,artist := range Template_data.Artists {
+		var id = artist.ID
+		By_Name.Add(artist.Name , artist.ID)
+		By_First_Album.Add(artist.FirstAlbum, artist.ID)
+		By_Creation_Date.Add(strconv.Itoa(artist.CreationDate), artist.ID)
+		// need a range 
+		for _, mem := range artist.Members{
+			By_Member.Add( mem , id)
+		}
+	}
+}
+var All_Search_Keys struct{
+	SearchKyes []string
+}
+
+func FillOutSearchKeys(){
+	var sl [][]string
+	sl = append(sl , By_Name.Retrieve())
+	sl = append(sl , By_Member.Retrieve())
+	sl = append(sl , By_First_Album.Retrieve())
+	sl = append(sl , By_Creation_Date.Retrieve())
 }
